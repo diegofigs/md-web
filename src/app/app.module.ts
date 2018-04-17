a37b45b28bbf7d6ad1fc734867a7728c131da303
Diego Figueroa <diego.fv94@gmail.com>
2018-04-17 15:50:12 -0400

    add markup for initial layout and init logic for app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppComponent } from './app.component';
+import { DocumentModule } from './document/document.module';
+import { ServicesModule } from './services/services.module';
 
 
 @NgModule({
@@ -10,7 +17,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatGridListModule,
+    MatSidenavModule,
+    DocumentModule,
+    ServicesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
