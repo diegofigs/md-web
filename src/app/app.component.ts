59189eee848abd841854466ab2941d0fdfd95e5a
Diego Figueroa <diego.fv94@gmail.com>
2018-04-19 11:26:15 -0400

    align app's editor and preview, sync selected document

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Markdown Editor';
+  selectedDocument = {
+    name: 'New Document\n',
+    contents: '# MD Editor\n' +
+    '## Getting started\n' +
+    'You can start typing away or choose one of the documents from the sidenav\n'
+  };
+
+  passDocumentToEditor(doc) {
+    this.selectedDocument = doc;
+  }
 }
