daf732fc02a292304848ce7714c58184302d3863
Diego Figueroa <diego.fv94@gmail.com>
2018-04-19 11:42:23 -0400

    add preview markup along with renderer

--- src/app/document/document-preview/document-preview.component.ts
+++ src/app/document/document-preview/document-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-document-preview',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentPreviewComponent implements OnInit {
 
+  @Input('changedDocument') changedDocument: any;
   constructor() { }
 
   ngOnInit() {
